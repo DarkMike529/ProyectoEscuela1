4c6aded05bd1a229c355e5e591b615f70c058ea7
DarkMike529 <darkmike529@gmail.com>
2025-07-18 10:58:55 -0600

    Here we going to it

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import models.Conexionn;
+import screens.VentanaPrincipal;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = null;
+        try {
+            conn = Conexionn.conectar();{
+                System.out.println("Conexion exitosa a la base de datos");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos" + e.getMessage());
+        }
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal("Proyecto Escuela", conn);
+        ventanaPrincipal.setSize(500,500);
+        ventanaPrincipal.setVisible(true);
+    }
+}
---
+++ src/screens/VentanaPrincipal.java
@@ -0,0 +1,93 @@
+package screens;
+
+import java.sql.Connection;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class VentanaPrincipal extends JFrame{
+    public JDesktopPane desktop;  // Escritorio dentro del Frame
+    private JMenuBar jMenuBar;
+    private JMenu jMenuAlumnos;
+    private JMenu jMenuCarreras;
+    private JMenuItem jMenuItemInsertar;
+    private JMenuItem jMenuItemVerAlumnos;
+    private JMenuItem jMenuItemInsertarCarreras;
+    private JMenuItem jMenuItemVerCarreras;
+
+    private Connection conn;
+
+    public VentanaPrincipal(String title, Connection conn){
+        this.conn = conn;
+        this.setTitle(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
+    }
+
+    private void initComponents(){
+        // DesktopPane - escritorio
+        desktop = new JDesktopPane();
+        this.add(desktop);
+        this.setContentPane(desktop); 
+        // Barra de Menu
+        jMenuBar = new JMenuBar();
+        // this.add(jMenuBar);
+        // Menu Alumnos
+        jMenuAlumnos = new JMenu();
+        jMenuAlumnos.setText("Alumnos");
+        // Elemento de menu: Insertar
+        jMenuItemInsertar = new JMenuItem();
+        jMenuItemInsertar.setText("Insertar...");
+        
+        jMenuItemInsertar.addActionListener(e -> jmiInsertarAlumno());
+
+        // Elemento de menu: Ver alumnos
+        jMenuItemVerAlumnos = new JMenuItem();
+        jMenuItemVerAlumnos.setText("Ver todos los alumnos...");
+
+        // Menú Carreras
+        jMenuCarreras = new JMenu();
+        jMenuCarreras.setText("Carreras");
+        // Elemento de menu: Ver Carreras
+        jMenuItemVerCarreras = new JMenuItem();
+        jMenuItemVerCarreras.setText("Ver todas las carreras...");
+        // Elemento de menu: Insertar carrera
+        jMenuItemInsertarCarreras = new JMenuItem();
+        jMenuItemInsertarCarreras.setText("Insertar...");
+
+        jMenuItemInsertarCarreras.addActionListener(e -> jmiInsertarCarreras());
+
+
+        // Agregar menuItem a menu 
+        jMenuAlumnos.add(jMenuItemVerAlumnos);
+        jMenuAlumnos.add(jMenuItemInsertar);
+        jMenuBar.add(jMenuAlumnos);
+
+        jMenuCarreras.add(jMenuItemVerCarreras);
+        jMenuCarreras.add(jMenuItemInsertarCarreras);
+        jMenuBar.add(jMenuCarreras);
+
+        this.setJMenuBar(jMenuBar);
+        pack();
+    }
+
+    private void jmiInsertarCarreras(){
+        //crear un objeto de tipo JinternalFrame
+        JInternalFrameInsertarCarrera insertarCarrera = new JInternalFrameInsertarCarrera(this.conn);
+        //agregar el internalframe al escritorio
+        this.desktop.add(insertarCarrera);
+        //le digo que se muestre el internal frame
+        insertarCarrera.setVisible(true);
+    }
+
+    private void jmiInsertarAlumno(){
+        JInternalFrameInsertarAlumnos insertarAlumno = new JInternalFrameInsertarAlumnos();
+        //agregar el internalframe al escritorio
+        this.desktop.add(insertarAlumno);
+        //le digo que se muestre el internal frame
+        insertarAlumno.setVisible(true);
+    }
+
+}
